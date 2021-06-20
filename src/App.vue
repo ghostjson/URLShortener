29f71edd4de3b166a20612aa4eca3110d02a2290
Akhil Ravindran <imakhilravindran@gmail.com>
2021-06-20 22:26:19 +0530

    feat: basic UI implemented

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-    <div></div>
+    <div>
+        <home></home>
+    </div>
 </template>
+
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        font-family: 'Roboto', sans-serif;
+        box-sizing: border-box;
+    }
+
+    :root{
+        --primary-color: #283646;
+        --secondary-color: #F65B53;
+        --complementary-color1: #10A3A9;
+        --complementary-color2: #F7A35F;
+    }
+</style>
 
 <script>
 
-export default {
-  name: 'App',
-  components: {
-  }
-}
+    import Home from "@/pages/Home/Home";
+    export default {
+        name: 'App',
+        components: {Home},
+    }
 </script>
 
---
+++ src/pages/Home/Home.vue
@@ -0,0 +1,31 @@
+<script>
+    export default {
+        name: 'Home',
+    }
+</script>
+
+<template>
+    <div id="home">
+        <div class="content">
+            <div class="logo">
+                <img src="../../assets/logo_white_croppped.png" alt="easeapp-logo">
+            </div>
+            <div class="title">
+                <h3>URL SHORTENER</h3>
+            </div>
+            <p class="subtitle">
+                Paste your long long URL to be SHORTENED.
+            </p>
+
+            <form>
+                <label for="link" style="display: none">Link</label>
+                <input type="text" id="link" placeholder="Paste link here">
+                <br>
+                <button type="button">short it</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<style src="./home.css" scoped></style>
+
